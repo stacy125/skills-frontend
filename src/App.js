33c034816279b01087021e56a9e7af7fe9b3bb68
stacy leitstein <unique523@yahoo.com>
2021-09-10 13:39:57 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Cards from "./Cards"
+import Skills from "./Skills"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Cards />
+      <Skills />
     </div>
   );
 }
